44efec84280c96c9f89f767cc7c87f980c0ce6aa
kebinshorts <59887703+kebinshorts@users.noreply.github.com>
2020-12-20 17:30:27 -0500

    finished implementing chronometers
    made study and break chronometers fully functional

--- app/src/main/java/com/example/study_tracker/MainActivity.kt
+++ app/src/main/java/com/example/study_tracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.study_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.SystemClock
 import android.view.View
 import android.widget.Button
 import android.widget.Chronometer
@@ -15,28 +16,57 @@ class MainActivity : AppCompatActivity() {
         val breakChronometer = findViewById<Chronometer>(R.id.breakChronometer)
 
         val studyButton = findViewById<Button>(R.id.studyButton)
+        val breakButton = findViewById<Button>(R.id.breakButton)
 
         studyButton?.setOnClickListener(object : View.OnClickListener {
             var isRunning = false
 
             override fun onClick(v: View?) {
                 if (!isRunning) {
+                    studyChronometer.setBase(SystemClock.elapsedRealtime())
                     studyChronometer.start()
                     isRunning = true
-                }
-
-                else {
+                } else {
+                    studyChronometer.setBase(SystemClock.elapsedRealtime())
                     studyChronometer.stop()
+                    breakChronometer.setBase(SystemClock.elapsedRealtime())
+                    breakChronometer.stop()
                     isRunning = false
                 }
 
-                studyButton.setText(if (isRunning) R.string.button_study_start else R.string.button_study_stop )
+                if (isRunning) {
+                    breakChronometer.isEnabled = true
+                    breakButton.isEnabled = true
+                }
+                else {
+                    breakChronometer.isEnabled = false
+                    breakButton.isEnabled = false
+                }
 
-                TODO("add toast")
+                studyButton.setText(if (isRunning) R.string.button_study_stop else R.string.button_study_start)
+
             }
 
         })
 
-        val breakButton = findViewById<Button>(R.id.breakButton)
+        breakButton?.setOnClickListener(object : View.OnClickListener {
+            var isRunning = false
+
+            override fun onClick(v: View?) {
+                if (!isRunning) {
+                    breakChronometer.setBase(SystemClock.elapsedRealtime())
+                    breakChronometer.start()
+                    isRunning = true
+                } else {
+                    breakChronometer.setBase(SystemClock.elapsedRealtime())
+                    breakChronometer.stop()
+                    isRunning = false
+                }
+
+                breakButton.setText(if (isRunning) R.string.button_break_stop else R.string.button_break_start)
+
+            }
+
+        })
     }
 }
