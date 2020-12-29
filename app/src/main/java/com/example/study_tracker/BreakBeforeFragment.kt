d68c0c76521e68b303f719b9be669cf2dd96c285
Sookeong Cho <skcho@uwaterloo.ca>
2020-12-29 12:35:28 -0500

    Update break before fragment and xml file

--- app/src/main/java/com/example/study_tracker/BreakBeforeFragment.kt
+++ app/src/main/java/com/example/study_tracker/BreakBeforeFragment.kt
@@ -10,7 +10,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_home.view.*
-//import kotlinx.android.synthetic.main.activity_home.view.breakButton
 import kotlinx.android.synthetic.main.fragment_break_before.*
 import kotlinx.android.synthetic.main.fragment_break_before.view.*
 
@@ -29,23 +28,6 @@ class BreakBeforeFragment : Fragment() {
         v.breakButton.setOnClickListener {
             comm.passDataCom(v.editTextBreakNotes.text.toString())
         }
-
-        /*
-        val breakButton = v.findViewById<Button>(R.id.breakButton)
-
-        breakButton.setOnClickListener(object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                var userNotes = editTextBreakNotes.text.toString()
-
-                val intent = Intent(activity, HomeActivity::class.java)
-                intent.putExtra(userNotes, editTextBreakNotes)
-
-                startActivity(intent);
-                }
-
-            }
-        })
-        */
 
         return v
     }
