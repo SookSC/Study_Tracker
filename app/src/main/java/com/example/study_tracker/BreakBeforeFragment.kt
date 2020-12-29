0e6c6a89a90b8eca607fd3b33069cde52d507540
Sookeong Cho <skcho@uwaterloo.ca>
2020-12-29 11:42:37 -0500

    Rename BreakDurationFragment.kt to BreakDurationFragment.kt and fragment_break_duration.xml to fragment_break_during.xml

--- app/src/main/java/com/example/study_tracker/BreakBeforeFragment.kt
+++ app/src/main/java/com/example/study_tracker/BreakBeforeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.study_tracker
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -8,26 +9,43 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_home.view.*
+//import kotlinx.android.synthetic.main.activity_home.view.breakButton
 import kotlinx.android.synthetic.main.fragment_break_before.*
+import kotlinx.android.synthetic.main.fragment_break_before.view.*
 
 private lateinit var viewOfLayout: View
 
 class BreakBeforeFragment : Fragment() {
+    lateinit var comm: Communicator
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val v = inflater.inflate(R.layout.fragment_break_before, container, false)
 
+        comm = activity as Communicator
+
+        v.breakButton.setOnClickListener {
+            comm.passDataCom(v.editTextBreakNotes.text.toString())
+        }
+
+        /*
         val breakButton = v.findViewById<Button>(R.id.breakButton)
 
         breakButton.setOnClickListener(object : View.OnClickListener{
             override fun onClick(v: View?) {
+                var userNotes = editTextBreakNotes.text.toString()
 
-                var editTextBreakNotes = editTextBreakNotes.text.toString()
+                val intent = Intent(activity, HomeActivity::class.java)
+                intent.putExtra(userNotes, editTextBreakNotes)
+
+                startActivity(intent);
+                }
 
             }
         })
-
+        */
 
         return v
     }
--- app/src/main/java/com/example/study_tracker/BreakDuringFragment.kt
+++ app/src/main/java/com/example/study_tracker/BreakDuringFragment.kt
@@ -9,13 +9,13 @@ import android.view.WindowManager
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_break_duration.view.*
+import kotlinx.android.synthetic.main.fragment_break_during.view.*
 
-class BreakDurationFragment : Fragment() {
+class BreakDuringFragment : Fragment() {
     var inputText: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val v = inflater.inflate(R.layout.fragment_break_duration, container, false)
+        val v = inflater.inflate(R.layout.fragment_break_during, container, false)
         inputText = arguments?.getString("input_txt").toString()
 
         v.output_textview.text = inputText
