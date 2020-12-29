130aea1e536a4744209aac6398208aa23c1aaed5
Sookeong Cho <skcho@uwaterloo.ca>
2020-12-29 13:20:28 -0500

    Update break during fragment and xml file

--- app/src/main/java/com/example/study_tracker/BreakDuringFragment.kt
+++ app/src/main/java/com/example/study_tracker/BreakDuringFragment.kt
@@ -7,18 +7,41 @@ import android.view.View
 import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Button
+import android.widget.Chronometer
+import android.os.SystemClock
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.fragment_break_during.*
 import kotlinx.android.synthetic.main.fragment_break_during.view.*
+import kotlinx.android.synthetic.main.fragment_break_during.view.breakDuringButton
+import androidx.fragment.app.FragmentTransaction
+import kotlinx.android.synthetic.main.fragment_break_before.view.*
+
 
 class BreakDuringFragment : Fragment() {
     var inputText: String = ""
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val v = inflater.inflate(R.layout.fragment_break_during, container, false)
-        inputText = arguments?.getString("input_txt").toString()
+        inputText = arguments?.getString("breakInputText").toString()
 
         v.output_textview.text = inputText
+
+        val breakChronometer = v.findViewById<Chronometer>(R.id.breakChronometer)
+        val breakDuringButton = v.findViewById<Button>(R.id.breakDuringButton)
+
+        breakChronometer.setBase(SystemClock.elapsedRealtime())
+        breakChronometer.start()
+
+        //Close this fragment on button click
+        lateinit var comm: Communicator
+        comm = activity as Communicator
+
+        v.breakDuringButton.setOnClickListener {
+            breakChronometer.setBase(SystemClock.elapsedRealtime())
+            breakChronometer.stop()
+            comm.openBreakBeforeFragmentCom()
+        }
 
         return v
     }
