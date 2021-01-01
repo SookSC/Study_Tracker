66b80576e3bf18ac598f0419186506805bd3f1cc
kebinshorts <59887703+kebinshorts@users.noreply.github.com>
2021-01-01 12:32:49 -0500

    added guidelines
    added guidelines

--- app/src/main/java/com/example/study_tracker/HomeActivity.kt
+++ app/src/main/java/com/example/study_tracker/HomeActivity.kt
@@ -19,64 +19,6 @@ class HomeActivity : AppCompatActivity(), Communicator {
 
         val fragmentStudyBefore = StudyBeforeFragment()
         supportFragmentManager.beginTransaction().replace(R.id.fragment_container_top, fragmentStudyBefore).commit()
-
-
-        /*val studyChronometer = findViewById<Chronometer>(R.id.studyChronometer)
-        val breakChronometer = findViewById<Chronometer>(R.id.breakChronometer)
-
-        val studyButton = findViewById<Button>(R.id.studyButton)
-        val breakButton = findViewById<Button>(R.id.breakButton)
-
-        studyButton?.setOnClickListener(object : View.OnClickListener {
-            var isRunning = false
-
-            override fun onClick(v: View?) {
-                if (!isRunning) {
-                    studyChronometer.setBase(SystemClock.elapsedRealtime())
-                    studyChronometer.start()
-                    isRunning = true
-                } else {
-                    studyChronometer.setBase(SystemClock.elapsedRealtime())
-                    studyChronometer.stop()
-                    breakChronometer.setBase(SystemClock.elapsedRealtime())
-                    breakChronometer.stop()
-                    isRunning = false
-                }
-
-                if (isRunning) {
-                    breakChronometer.isEnabled = true
-                    breakButton.isEnabled = true
-                }
-                else {
-                    breakChronometer.isEnabled = false
-                    breakButton.isEnabled = false
-                }
-
-                studyButton.setText(if (isRunning) R.string.button_study_stop else R.string.button_study_start)
-
-            }
-
-        })
-
-        breakButton?.setOnClickListener(object : View.OnClickListener {
-            var isRunning = false
-
-            override fun onClick(v: View?) {
-                if (!isRunning) {
-                    breakChronometer.setBase(SystemClock.elapsedRealtime())
-                    breakChronometer.start()
-                    isRunning = true
-                } else {
-                    breakChronometer.setBase(SystemClock.elapsedRealtime())
-                    breakChronometer.stop()
-                    isRunning = false
-                }
-
-                breakButton.setText(if (isRunning) R.string.button_break_stop else R.string.button_break_start)
-
-            }
-
-        })*/
     }
 
     override fun comPassStudyNotes(textInputStudyNotes: String) {
