f9307c34f58958a931dcceb75c6c05542cdaf7d6
SookSC <36337261+SookSC@users.noreply.github.com>
2021-01-01 13:52:23 -0500

    Create log activity

---
+++ app/src/main/java/com/example/study_tracker/LogActivity.kt
@@ -0,0 +1,11 @@
+package com.example.study_tracker
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LogActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log)
+    }
+}
