f5c5da5dc6b27c2492220f717579988e57590e65
SookSC <36337261+SookSC@users.noreply.github.com>
2021-01-05 13:11:06 -0500

    Check if log directory and text file exist and create them if they do not exist

--- app/src/main/java/com/example/study_tracker/LogActivity.kt
+++ app/src/main/java/com/example/study_tracker/LogActivity.kt
@@ -2,14 +2,74 @@ package com.example.study_tracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import java.io.File
+import java.io.FileInputStream
 
 class LogActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log)
 
-        val fileName = "StudyTrackerLogData.txt"
+        val SD_main = getExternalFilesDir("/StudyTimerData")
+
+        var createSuccess = true
+
+        // Create directory if it does not already exist
+        if(!SD_main!!.exists()) {
+            createSuccess = SD_main.mkdir()
+        }
+
+        // Directory already exists or creation of directory is successful
+        if(createSuccess) {
+            val logFile = File("StudyTimerLogData.txt")
+
+            // Create text file if it does not already exist
+            if(!logFile.exists()){
+                createSuccess = logFile.mkdir()
+            }
+
+            // Text file already exists or creation of text file is successful
+            if(createSuccess) {
+                // Directory and text file exist!
+                // For testing:
+                Toast.makeText(this@LogActivity,"Log directory and text file exist",
+                    Toast.LENGTH_SHORT).show()
+            }
+
+            // Text file creation not successful
+            else {
+                // For testing:
+                Toast.makeText(this@LogActivity,"Text file creation UNSUCCESSFUL",
+                    Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Directory creation not successful
+        else {
+            Toast.makeText(this@LogActivity,"Directory creation UNSUCCESSFUL",
+                Toast.LENGTH_SHORT).show()
+        }
+
+        /*
+        val path = getExternalFilesDir(null)
+
+        val studyTimerDirectory = File(path, "StudyTimer")
+        studyTimerDirectory.mkdirs()
+        val file = File(studyTimerDirectory, "StudyTimerLogData.txt")
+
+        //Write
+        file.appendText("Record goes here")
+
+        //Read
+        val inputAsString = FileInputStream(file).bufferedReader().use { it.readText() }
+         */
+
+
+
+
+        /*
+        val fileName = "StudyTimerLogData.txt"
         
         var file = File(fileName)
 
@@ -19,10 +79,13 @@ class LogActivity : AppCompatActivity() {
             //Do nothing
         }
         else {
+
             //Create file
         }
 
         //Open file
         //Read/write to file
+
+         */
     }
 }
